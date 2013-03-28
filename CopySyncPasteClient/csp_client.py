c4476fa3c91186fe7ce85560c2c10920a12e0f6b
aravindhramu <aravindhramu@gmail.com>
2013-03-28 12:16:48 +0530

    exception handling in client

--- CopySyncPasteClient/csp_client.py
+++ CopySyncPasteClient/csp_client.py
@@ -39,19 +39,24 @@ def discover():
 def findServicePort(devices,device_name,service_name):
     req_device = ""
     services = []
-    for device in devices:
-        if bluetooth.lookup_name(device) == device_name:
-            services = bluetooth.find_service(address=device)
-            req_device = device
-    if services == None:
-	print "required service not found in mobile."
-	exit(1)
-    for service in services:
-        if service['name'] == None:
-            continue
-        if 'name' in service and service['name'].find(service_name) != -1:
-            if service['port'] != None:
-                return req_device,service['port']
+    try:
+        for device in devices:
+            if bluetooth.lookup_name(device) == device_name:
+                services = bluetooth.find_service(address=device)
+                req_device = device
+        if req_device == "":
+            raise BluetoothError
+        if services == []:
+            return req_device,None
+        for service in services:
+            if service['name'] == None or service == None:
+                continue
+            if 'name' in service and service['name'].find(service_name) != -1:
+                if service['port'] != None:
+                    return req_device,service['port']
+        return req_device,None
+    except Exception as e:
+        return None,None
 
 def getClipBoardContents():
     clipboard = gtk.clipboard_get()
@@ -67,36 +72,39 @@ if __name__ == "__main__":
         exit(1)
     device,port = findServicePort(devices,DEVICE_NAME,"OBEX")
     if device == None:
-        print "Please turn on bluetooth in the devices around you."
+        print "Device \""+DEVICE_NAME+"\" doesn't have Bluetooth on"
         exit(1)
     if port == None:
         print "Required service not present in device.Cannot run application"
         exit(1)
     client = BrowserClient(device,port)
     print "Connecting.."
-    client.connect()
+    try:
+        client.connect()
+    except Exception as e:
+        print "Cannot establish connection.Aborting.."
     print "Connected.."
     contents = getClipBoardContents()
-    print "Sending Clipboard contents..."
     client.setpath(FOLDER_IN_DEVICE)
     client.put(FILE_NAME,contents)
-    client.disconnect()
     device,port = findServicePort(devices,DEVICE_NAME,"btserver")
     if device == None:
-        print "Please turn on bluetooth in the devices around you."
+        print "Device \""+DEVICE_NAME+"\" doesn't have Bluetooth on"
         exit(1)
     if port == None:
         print "Required service not present in device.Cannot run application"
         exit(1)
     try:
-	    client_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-	    client_sock.connect((device,port))
-	    client_sock.send(FOLDER_IN_DEVICE+"/"+FILE_NAME+'\n')
+        client_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        client_sock.connect((device,port))
+        print "Sending Clipboard contents..."
+        client_sock.send(FOLDER_IN_DEVICE+"/"+FILE_NAME+'\n')
     except Exception as e:
-            print "ERROR: Cannot establish connection !"+str(e)
-            exit(1)
+        print "ERROR: Cannot establish connection !"+str(e)
+        exit(1)
     #time.sleep(120)
     client_sock.close()
+    client.disconnect()
     print "Done."
 
 
